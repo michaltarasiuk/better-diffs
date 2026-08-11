0961be23fbfc8b9a8fa1c9dc20384656b0b874b3
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-11 16:53:56 +0200

    Update cleanup logic to skip execution on Vercel environment

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' && !process.env.VERCEL) {
     const {deleteExpired} = await import('./lib/db/cleanup');
     deleteExpired(24);
     setInterval(() => deleteExpired(24), 60 * 60 * 1000);
