bcf2ba7aade386ef9e1ad849e2107c3b5cbf7b7c
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-11 18:16:51 +0200

    Add simple-import-sort config from hono-vitals

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import {defineConfig, globalIgnores} from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -9,6 +10,31 @@ const eslintConfig = defineConfig([
   {
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'],
+            ['^node:'],
+            ['^@?\\w.*\\u0000$'],
+            ['^@?\\w'],
+            ['^@/.*\\u0000$'],
+            ['^@/'],
+            ['^\\..*\\u0000$'],
+            ['^\\.'],
+            ['^'],
+          ],
+        },
+      ],
     },
   },
 ]);
