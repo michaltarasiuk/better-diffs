1621c4264aff209a015ed48015fdf118699ea9e7
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-12 15:21:41 +0200

    Enhance ESLint configuration by adding rules for consistent type definitions, type imports, and no import type side effects

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,21 @@ const eslintConfig = defineConfig([
   globalIgnores(['.next/**', 'next-env.d.ts']),
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
   {
