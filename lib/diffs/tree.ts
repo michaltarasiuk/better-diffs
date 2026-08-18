34e1f2a35c0768f8b6ccf8ca6236610350c980f2
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-18 21:34:05 +0200

    Remove expand-matches from diff tree search mode

--- lib/diffs/tree.ts
+++ lib/diffs/tree.ts
@@ -10,7 +10,6 @@ import {
 
 const DIFF_TREE_OPTIONS = {
   id: 'diff-file-tree',
-  fileTreeSearchMode: 'expand-matches',
   flattenEmptyDirectories: true,
   initialExpansion: 'open',
   initialVisibleRowCount: Infinity,
