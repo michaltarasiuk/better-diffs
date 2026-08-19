bafb4cd965d342b5e492c86b426b6d116e2ba116
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-19 21:24:54 +0200

    Use line-info-basic hunk separators in diff viewer

--- lib/diffs/options.ts
+++ lib/diffs/options.ts
@@ -2,4 +2,5 @@ import {DEFAULT_THEMES, type FileDiffOptions} from '@pierre/diffs';
 
 export const DIFF_VIEWER_OPTIONS = {
   theme: DEFAULT_THEMES,
+  hunkSeparators: 'line-info-basic',
 } satisfies FileDiffOptions<unknown>;
