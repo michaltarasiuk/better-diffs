5ee37a397c75642d050f40ec858a7f76a2778a93
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-26 13:10:58 +0200

    Retake README screenshots without react-grab dev tools

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import './_globals.css';
 
-import Script from 'next/script';
-
 import {Providers} from '@/app/_providers';
-import {env} from '@/lib/env';
 
 import type {Metadata} from 'next';
 
@@ -14,15 +11,6 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: LayoutProps<'/'>) {
   return (
     <html lang="en">
-      <head>
-        {env.NODE_ENV === 'development' && (
-          <Script
-            src="//unpkg.com/react-grab/dist/index.global.js"
-            strategy="beforeInteractive"
-            crossOrigin="anonymous"
-          />
-        )}
-      </head>
       <body className="bg-background text-foreground">
         <Providers>{children}</Providers>
       </body>
