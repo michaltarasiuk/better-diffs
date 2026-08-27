90e252720134fa2ddcb98da20ca04fad579d31f6
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-27 20:08:45 +0200

    Add page show hook to handle bfcache persistence in Providers component

--- app/_providers.tsx
+++ app/_providers.tsx
@@ -2,10 +2,18 @@
 
 import {NuqsAdapter} from 'nuqs/adapters/next/app';
 
+import {usePageShow} from '@/lib/hooks/use-page-show';
+
 interface ProvidersProps {
   readonly children: React.ReactNode;
 }
 
 export function Providers({children}: ProvidersProps) {
+  usePageShow(function bypassBfcache(e) {
+    if (e.persisted) {
+      window.location.reload();
+    }
+  });
+
   return <NuqsAdapter>{children}</NuqsAdapter>;
 }
---
+++ lib/hooks/use-page-show.ts
@@ -0,0 +1,11 @@
+import {useEffect, useEffectEvent} from 'react';
+
+export function usePageShow(onPageShow: (e: PageTransitionEvent) => void) {
+  const onPageShowEvent = useEffectEvent(onPageShow);
+  useEffect(() => {
+    window.addEventListener('pageshow', onPageShowEvent);
+    return () => {
+      window.removeEventListener('pageshow', onPageShowEvent);
+    };
+  }, []);
+}
