cc94e0a9a4df855d5dd5bd62992e05c5079bd774
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-09-15 23:59:13 +0200

    Refactor isIterable function to improve readability

--- utils/iterable.ts
+++ utils/iterable.ts
@@ -3,6 +3,7 @@ import {isDefined} from './defined';
 export function isIterable<T>(value: unknown): value is Iterable<T> {
   return (
     isDefined(value) &&
-    typeof (value as Iterable<T>)[Symbol.iterator] === 'function'
+    Symbol.iterator in value &&
+    value[Symbol.iterator] === 'function'
   );
 }
